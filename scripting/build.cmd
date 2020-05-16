a90629e95bc0e950804ecd0b2bd79fde78790f59
Adam Short <adamconnorshort@gmail.com>
2020-05-16 12:37:52 -0400

    Updated build

--- scripting/build.cmd
+++ scripting/build.cmd
@@ -3,3 +3,4 @@
 spcomp jailbreak.sp -o ..\plugins\jailbreak.smx
 spcomp markers.sp -o ..\plugins\markers.smx
 spcomp specialday.sp -o ..\plugins\specialday.smx
+spcomp mike.sp -o ..\plugins\mike.smx
